35a02ff0e1c2731541f7dfa8fce3299efaf73de9
Santhosh <santhoshdm@Santhoshs-MacBook-Air.local>
2020-04-14 12:36:04 +0530

    post_deploy.sh

--- post_deploy.sh
+++ post_deploy.sh
@@ -1,2 +1,2 @@
 kubectl describe service my-nginx-nginx-ingress-controller | grep "LoadBalancer Ingress" | awk '{print $3}' > EIP
-wget --quiet --tries=2 http://"$EIP":80 || exit 1
+wget --quiet --tries=1 http://"$EIP":80 || exit 0
