3d7f680ab70cea302d7d9aa6abb9bceb28952963
Santhosh <santhoshdm@Santhoshs-MacBook-Air.local>
2020-04-14 13:26:19 +0530

    printing output

--- post_deploy.sh
+++ post_deploy.sh
@@ -1,7 +1,7 @@
 touch ./eip.txt
 kubectl describe service my-nginx-nginx-ingress-controller | grep "LoadBalancer Ingress" | awk '{print $3}' > ./eip.txt
 new_var=$(cat ./eip.txt)
-cat $new_var
+cat "$new_var"
 curl http://"$new_var":80
 #touch ./op.txt
 ##wget http://"$EIP":80 -o ./op.txt && cat ./op.txt
